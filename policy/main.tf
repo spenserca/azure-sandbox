4edcc4f14bf9bbb473ed87700ef605996e5e14f3
Spenser Andreassen <spenserca@gmail.com>
2021-02-26 16:12:18 -0600

    policy project deploying resource group

---
+++ policy/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "azure-sandbox-tf"
+    storage_account_name = "scaazsbxtfstate"
+    container_name       = "az-sandbox-tf-state"
+    key                  = "prod.terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg_policy" {
+  location = "Central US"
+  name     = "spenserca-policy"
+}
