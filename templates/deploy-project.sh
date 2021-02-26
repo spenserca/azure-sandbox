267bec381de50542b95cb71e38c9c9777ab287d6
Spenser Andreassen <spenserca@gmail.com>
2021-02-26 16:11:50 -0600

    fix deploy script use terraform gitignore

--- templates/deploy-project.sh
+++ templates/deploy-project.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-PROJECT_NAME="$1"
-
-if [ -z "$PROJECT_NAME" ]; then echo "[ERROR] Pass the project name as the first parameter"; exit 1; fi
-
 echo "Initializing terraform in project"
-terraform init "$PROJECT_NAME"
+terraform init
 
 echo "Generating terraform plan"
-terraform plan "$PROJECT_NAME"
+terraform plan
+
+echo "Deploying terraform plan"
+terraform apply
